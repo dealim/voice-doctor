e91290827e15bf55e1503c74b58800888072551d
dealim <136411152+dealim@users.noreply.github.com>
2023-11-21 18:17:08 +0900

    feat: 페이드 아웃

--- static/js/script.js
+++ static/js/script.js
@@ -2,108 +2,120 @@
 
 // 첫 페이지 로드
 document.addEventListener('DOMContentLoaded', (event) => {
-    fetch('/main')
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById('dynamicContent').innerHTML = html;
-        });
+    loadContent('/main');
 });
 document.getElementById('dynamicContent').addEventListener('click', function (event) {
-    // event.target을 사용하여 클릭된 요소를 확인합니다.
     if (event.target.id === 'viewTextSummary') {
         event.preventDefault();
-        fetch('/show/voicetext')
-            .then(response => response.text())
-            .then(html => {
-                document.getElementById('dynamicContent').innerHTML = html;
-            });
+        loadContent('/show/voicetext');
     } else if (event.target.id === 'startEmotionAnalysis') {
         event.preventDefault();
-        fetch('/show/emotion')
+        loadContent('/show/emotion');
+    }
+});
+
+// 페이드 아웃 및 새 콘텐츠 로드 함수
+function loadContent(url) {
+    const dynamicContent = document.getElementById('dynamicContent');
+    // 페이드 아웃
+    dynamicContent.classList.add('hidden');
+
+    // CSS 트랜지션을 기다립니다
+    setTimeout(() => {
+        fetch(url)
             .then(response => response.text())
             .then(html => {
-                document.getElementById('dynamicContent').innerHTML = html;
+                // 콘텐츠 업데이트 및 페이드 인
+                dynamicContent.innerHTML = html;
+                dynamicContent.classList.remove('hidden');
+                // 필요한 경우 추가 초기화 함수 호출
+                if (url === '/main') {
+                    initializeFileDragAndDrop();
+                }
             });
-    }
-});
+    }, 300); // CSS 트랜지션 시간과 일치
+}
 
 // 보이스 넣기
-const dropArea = document.getElementById('dropArea');
-const fileInput = document.getElementById('fileInput');
-const fileInputLink = document.getElementById('fileInputLink');
+function initializeFileDragAndDrop() {
+    const dropArea = document.getElementById('dropArea');
+    const fileInput = document.getElementById('fileInput');
+    const fileInputLink = document.getElementById('fileInputLink');
 
-// 파일 입력 필드 열기
-fileInputLink.addEventListener('click', (e) => {
-    e.preventDefault();
-    fileInput.click();
-});
+    // 파일 입력 필드 열기
+    fileInputLink.addEventListener('click', (e) => {
+        e.preventDefault();
+        fileInput.click();
+    });
 
-// 드래그 이벤트 방지
-['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
-    dropArea.addEventListener(eventName, preventDefaults, false);
-});
+    // 드래그 이벤트 방지
+    ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+        dropArea.addEventListener(eventName, preventDefaults, false);
+    });
 
-function preventDefaults(e) {
-    e.preventDefault();
-    e.stopPropagation();
-}
-
-// 드래그 활성화/비활성화 스타일
-['dragenter', 'dragover'].forEach(eventName => {
-    dropArea.addEventListener(eventName, highlight, false);
-});
-
-['dragleave', 'drop'].forEach(eventName => {
-    dropArea.addEventListener(eventName, unhighlight, false);
-});
-
-function highlight(e) {
-    dropArea.classList.add('active');
-}
-
-function unhighlight(e) {
-    dropArea.classList.remove('active');
-}
-
-// 파일 드롭 처리
-dropArea.addEventListener('drop', handleDrop, false);
-let isFileUploaded = false;
-
-function handleDrop(e) {
-    if (isFileUploaded) {
-        return; // 파일이 이미 업로드된 경우, 추가 처리 방지
+    function preventDefaults(e) {
+        e.preventDefault();
+        e.stopPropagation();
     }
-    let dt = e.dataTransfer;
-    let files = dt.files;
 
-    handleFiles(files);
-}
+    // 드래그 활성화/비활성화 스타일
+    ['dragenter', 'dragover'].forEach(eventName => {
+        dropArea.addEventListener(eventName, highlight, false);
+    });
 
-function handleFiles(files) {
-    ([...files]).forEach(uploadFile);
-}
+    ['dragleave', 'drop'].forEach(eventName => {
+        dropArea.addEventListener(eventName, unhighlight, false);
+    });
 
-function uploadFile(file) {
-    let formData = new FormData();
-    formData.append('file', file); // 'file'은 서버에서 받을 때 사용할 키
+    function highlight(e) {
+        dropArea.classList.add('active');
+    }
 
-    fetch('/api/upload', {
-        method: 'POST',
-        body: formData
-    })
-        .then(response => response.json())
-        .then(data => {
-            if (data.message === 'File uploaded successfully!') {
-                console.log(data);
-                isFileUploaded = true; // 업로드 상태 업데이트
-                document.getElementById('dropAreaMessage').innerText = "업로드가 완료 되었습니다"; // 메시지 변경
-                dropArea.classList.add('uploaded'); // 업로드된 상태 스타일 적용
-            } else {
-                document.getElementById('dropAreaMessage').innerText = "업로드 실패";
-            }
+    function unhighlight(e) {
+        dropArea.classList.remove('active');
+    }
+
+    // 파일 드롭 처리
+    dropArea.addEventListener('drop', handleDrop, false);
+    let isFileUploaded = false;
+
+    function handleDrop(e) {
+        if (isFileUploaded) {
+            return; // 파일이 이미 업로드된 경우, 추가 처리 방지
+        }
+        let dt = e.dataTransfer;
+        let files = dt.files;
+
+        handleFiles(files);
+    }
+
+    function handleFiles(files) {
+        ([...files]).forEach(uploadFile);
+    }
+
+    function uploadFile(file) {
+        let formData = new FormData();
+        formData.append('file', file); // 'file'은 서버에서 받을 때 사용할 키
+
+        fetch('/api/upload', {
+            method: 'POST',
+            body: formData
         })
-        .catch(error => {
-            console.error('Error:', error);
-        });
+            .then(response => response.json())
+            .then(data => {
+                if (data.message === 'File uploaded successfully!') {
+                    console.log(data);
+                    isFileUploaded = true; // 업로드 상태 업데이트
+                    document.getElementById('dropAreaMessage').innerText = "업로드가 완료 되었습니다"; // 메시지 변경
+                    dropArea.classList.add('uploaded'); // 업로드된 상태 스타일 적용
+                } else {
+                    document.getElementById('dropAreaMessage').innerText = "업로드 실패";
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }
 }
+
 
