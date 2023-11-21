defccef3d4e5db2a08924bad54bc0f3a631a54b0
minji0916 <a2927036@naver.com>
2023-11-22 02:05:54 +0900

    의사, 환자 담화 분리하여 json 파일 생성, error : flask에서 json type return

---
+++ app_chart.py
@@ -0,0 +1,24 @@
+# _*_ coding: utf-8 _*_
+
+from flask import Flask, request, render_template
+import json
+from services.text_emotion_analysis import json_analyze_sentiment
+
+app = Flask(__name__)
+
+@app.route('/')
+def view():
+    # 환자용 JSON 파일 읽기 & 각 문장에 대해 감정 분석 수행
+    patient = json_analyze_sentiment('./services/patient_text_request.json')
+            
+    # 의사용 JSON 파일 읽기 & 각 문장에 대해 감정 분석 수행
+    doctor = json_analyze_sentiment('./services/doctor_text_request.json')
+    
+    print(patient[0])
+    print()
+    print(doctor[0])
+    
+    return render_template('chartjs.html', patient=patient, doctor=doctor)
+
+if __name__ == '__main__':
+    app.run()
--- services/text_emotion_analysis.py
+++ services/text_emotion_analysis.py
@@ -1,17 +1,27 @@
 from google.cloud import language_v2
 import json
-import settings
+
+"""text_emotion_analysis.py를 실행하고 싶은 경우"""
+# from settings import get_projectId, get_secret
+
+"""app.py를 실행하고 싶은 경우"""
+from .settings import get_projectId, get_secret
 
 # SECRET KEY 가져오기 
-SECRET_API_KEY = settings.get_secret("APIKEY_TEXT_EMOTION_ANALYSIS")
-PROJECT_ID = settings.get_projectId()
+SECRET_API_KEY = get_secret("APIKEY_TEXT_EMOTION_ANALYSIS")
+PROJECT_ID = get_projectId()
 
-def analyze_sentiment(text_content) -> None:
+def analyze_sentiment(text_content):
     """
     문자열 내에서 감정 분석
 
     Args:
-      text_content: The text content to analyze.
+      text_content: 한 화자의 담화 문장들
+      
+    return:
+        dict안에 다음의 변수들이 묶어서 들어감
+            int : doc_sentiment_score, doc_sentiment_magnitude
+            list : text_contents, senti_scores, senti_magnitude
     """
    
     client = language_v2.LanguageServiceClient(
@@ -42,6 +52,27 @@ def analyze_sentiment(text_content) -> None:
     # input 문장들의 종합적인 sentiment score, manitude
     doc_sentiment_score = response.document_sentiment.score
     doc_sentiment_magnitude = response.document_sentiment.magnitude
+    
+    # input 문장들 각각에 대한 sentiment score, manitude
+    text_contents = []
+    senti_scores = []
+    senti_magnitudes = []
+    for sentence in response.sentences:
+        text_contents.append(sentence.text.content)
+        senti_scores.append(sentence.sentiment.score)
+        senti_magnitudes.append(sentence.sentiment.magnitude)
+    
+    # json 형식으로 묶어서 return
+    sentiment_result = {
+        "doc_sentiment_score" : doc_sentiment_score,
+        "doc_sentiment_magnitude" : doc_sentiment_magnitude,
+        "text_contents" : text_contents,
+        "senti_scores" : senti_scores,
+        "senti_magnitudes" : senti_magnitudes 
+    }
+    
+    """
+    # input 문장들의 종합적인 sentiment score, manitude
     print("="*50)
     print(f"전체 감정 score: {doc_sentiment_score}")
     print(f"전체 감정 magnitude: {doc_sentiment_magnitude}")
@@ -58,13 +89,31 @@ def analyze_sentiment(text_content) -> None:
     # the language specified in the request or, if not specified,
     # the automatically-detected language.
     print(f"언어: {response.language_code}")
+    """
+    
+    return sentiment_result
 
-# JSON 파일 읽기 예제
-with open('./services/patient_text_request.json', 'r', encoding='utf-8') as file:
-    json_data = json.load(file)
 
-# 각 문장에 대해 감정 분석 수행
-for result in json_data["results"]:
-    for alternative in result["alternatives"]:
-        text = alternative["transcript"]
-        analyze_sentiment(text)
+def json_analyze_sentiment(jsonfile):
+    """
+    Speech to text 파일을 읽고, analyze_sentiment 진행
+    
+    Args:
+        jsonfile : 감정 분석을 진행할 Speech to text를 마친 json 파일의 주소
+
+    Returns:
+        list : sentiment_results
+            하나의 화자마다 analyze_sentiment로 나온 dict 결과들을 list로 묶어서 반환
+    """
+    # JSON 파일 읽기 예제
+    with open(jsonfile, 'r', encoding='utf-8') as file:
+        json_data = json.load(file)
+
+    sentiment_results = []
+    # 각 문장에 대해 감정 분석 수행
+    for result in json_data["results"]:
+        for alternative in result["alternatives"]:
+            text = alternative["transcript"]
+            sentiment_results.append(analyze_sentiment(text))
+    
+    return sentiment_results
