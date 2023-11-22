352aa2ae9e74fc8513e2bf52d0e76d2baf2422e9
Seunggu <seunggu_35@naver.com>
2023-11-22 17:07:26 +0900

    summary api

--- services/summary.py
+++ services/summary.py
@@ -1,13 +1,18 @@
 # pip install google-cloud-aiplatform
 import os
+import json
 import vertexai
 from vertexai.language_models import TextGenerationModel
-from settings import get_secret
+from settings import get_secret, get_projectId
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = get_secret("SUMMARY")
+PROJ = get_projectId()
 
 def text_summarization(
         temperature: float,
         project_id: str,
         location: str,
+        text: str,
 ) -> str:
     """Summarization Example with a Large Language Model"""
 
@@ -22,22 +27,38 @@ def text_summarization(
 
     model = TextGenerationModel.from_pretrained("text-bison@001")
     response = model.predict(
-        """Provide a summary with about two sentences for the following conversation:""" + 
-        """Um-hum . Yeah. Hello , good morning . Good
-          morning . So , tell me what's going on . Uh , sure , so , um , I
-          woke up probably three or four days ago , which , uh , wheezing and short of breath .
-          Okay , any cough or chest pain ? I cough infrequently , but no ,
-          uh , chest pain . Have you been exposed to anyone with covid ?
-          Uh , no , and I also took a test , which was negative . Uh , is it getting
-          worse , or better ? Uh , it has been getting a lot worse
-""",
+        """Provide a summary with about two sentences for the following conversation:""" + text,
         **parameters,
     )
-    print(f"Response from Model: {response.text}")
+    # print(f"Response from Model: {response.text}")
 
     return response.text
 
 
+def json_analyze_sentiment(jsonfile):
+    """
+    Speech to text 파일을 읽고, summarize 진행
+    
+    Args:
+        jsonfile : 감정 분석을 진행할 Speech to text를 마친 json 파일의 주소
+
+    Returns:
+        list : summarize
+            하나의 화자마다 summarize로 나온 dict 결과들을 list로 묶어서 반환
+    """
+    # JSON 파일 읽기 예제
+    with open(jsonfile, 'r', encoding='utf-8') as file:
+        json_data = json.load(file)
+
+    summarize_results = []
+    # 각 문장에 대해 감정 분석 수행
+    for result in json_data["results"]:
+        for alternative in result["alternatives"]:
+            text = alternative["transcript"]
+            summarize_results.append(text_summarization(0.0, PROJ, 'us-central1', text))
+
+    return summarize_results
+
+
 if __name__ == "__main__":
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = get_secret("SUMMARY")
-    text_summarization(0.0, 'applicationteam02', 'us-central1')
+    json_analyze_sentiment("C:\\Users\\seunggu\\Desktop\\GCP-TEAM2\\services\\patient_text_request.json")
