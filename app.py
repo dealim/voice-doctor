0e40a04053b2223935df7f2458395fcf2ff8a7c3
dealim <136411152+dealim@users.noreply.github.com>
2023-11-22 16:02:48 +0900

    style: Add chart

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+from services.text_emotion_analysis import json_analyze_sentiment
 import os
 
 app = Flask(__name__)
@@ -23,7 +24,13 @@ def show_voicetext():
 
 @app.route('/show/emotion')
 def show_emotion():
-    return render_template('show_text_emotion_analysis.html')
+    # 환자용 JSON 파일 읽기 & 각 문장에 대해 감정 분석 수행
+    patient = json_analyze_sentiment('./services/patient_text_request.json')
+
+    # 의사용 JSON 파일 읽기 & 각 문장에 대해 감정 분석 수행
+    doctor = json_analyze_sentiment('./services/doctor_text_request.json')
+
+    return render_template('show_text_emotion_analysis.html', patient=patient[0], doctor=doctor[0])
 
 
 @app.route('/api/upload', methods=['POST'])
@@ -37,7 +44,6 @@ def upload_file():
         return jsonify({'message': 'File uploaded successfully!'})
     else:
         return jsonify({'message': 'No file part'})
-
 
 # execute app
 if __name__ == '__main__':
--- services/settings.py
+++ services/settings.py
@@ -1,14 +1,17 @@
 import os, json
 
-# secrets.json 파일 위치를 명시
-BASE_PATH = os.environ['PYTHONPATH']
-secret_file = os.path.join(BASE_PATH, 'keys\secrets.json')
+# 현재 파일의 디렉토리를 기준으로 상위 디렉토리의 경로를 구함
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.join(current_dir, '..')
+
+# 예시: 'keys/secrets.json' 파일의 경로를 상위 디렉토리 기준으로 구함
+secret_file = os.path.join(parent_dir, 'keys', 'secrets.json')
 
 with open(secret_file) as f:
     secrets = json.loads(f.read())
 
 def get_projectId():
-    return "appteam02"
+    return "appteam2"
 
 def get_secret(setting):
     """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
