7467978fecccaef9bd66fbf9336b8fd630e52138
Lerini98 <hyerin7259@naver.com>
2023-12-19 07:57:26 +0900

    feat : 언어 인식(한국어, 영어, 일본어) 기능 추가

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify, session, send_from_directory
 from services.text_emotion_analysis import get_json_sentiment
-from services.speech_to_text_copy import transcribe_audio
+from services.speech_to_text_lang import transcribe_audio
 from services.summary import text_summarization
 from services.document_ai_ocr import get_ocr_json
 from config import Config
--- services/speech_to_text_lang.py
+++ services/speech_to_text_lang.py
@@ -5,10 +5,12 @@ import io
 import os
 import time
 import json
+from flask import current_app
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.path.join(parent_dir, "keys/applicationteam02-cf34308f779b.json")
+
 
 def multi_transcribe_audio(filename, extname):
     client = speech.SpeechClient()
@@ -17,77 +19,67 @@ def multi_transcribe_audio(filename, extname):
     sample_rate = int(audio_info['sample_rate'])
     start_time = time.time()
     if (extname == 'wav'):
-        encoding = 'speech.RecognitionConfig.AudioEncoding.LINEAR16'
-    elif (extname == 'FLAC'):
-        encoding = 'speech.RecognitionConfig.AudioEncoding.FLAC'
+        file_encoding = speech.RecognitionConfig.AudioEncoding.LINEAR16
+    elif (extname == 'flac'):
+        file_encoding = speech.RecognitionConfig.AudioEncoding.FLAC
 
     # 언어는 기본 언어를 포함하여 최대 4개의 언어를 지원한다.
     first_lang = "en-US"
     alternate_languages = ["ko-KR", "ja-JP"]
-    with io.open(filename, "rb") as audio_file:
+    with io.open(file_path, "rb") as audio_file:
         content = audio_file.read()
 
     audio = speech.RecognitionAudio(content=content)
 
     config = speech.RecognitionConfig(
-        encoding=encoding,
+        encoding=file_encoding,
         sample_rate_hertz=sample_rate,
         audio_channel_count=1,
         language_code=first_lang,
         alternative_language_codes=alternate_languages,
     )
-    print("Waiting for operation to complete...")
+
     response = client.recognize(config=config, audio=audio)
     for i, result in enumerate(response.results):
         alternative = result.alternatives[0]
-        # print("-" * 20)
-        # print(f"First alternative of result {i}: {alternative}")
-        # print(f"Transcript: {alternative.transcript}")
-
-    return response.results, encoding, sample_rate, filename, extname
-
-def detect_language(multi_lan):
-
-    lang_code = multi_transcribe_audio()
-    for i, result in enumerate(lang_code):
 
         language_check = result.language_code
-    return language_check
+
+    current_app.logger.info("언어 인식 완료")
+    return file_encoding, sample_rate, language_check, content
+
 
 def transcribe_audio(filename, output_json_file, extname):
-    multi_lan = multi_transcribe_audio(filename,extname)
-    detect_lang = detect_language(multi_lan)
+    file_encoding, sample_rate, language_check, content = multi_transcribe_audio(filename, extname)
 
     client = speech_v1.SpeechClient()
-    # start_time = time.time()
-    with io.open(filename, "rb") as audio_file:
-        content = audio_file.read()
 
     audio = {"content": content}
-    if (extname == 'speech.RecognitionConfig.AudioEncoding.LINEAR16'):
+    if (file_encoding == speech.RecognitionConfig.AudioEncoding.LINEAR16):
         encoding = 'LINEAR16'
-    elif (extname == 'speech.RecognitionConfig.AudioEncoding.FLAC'):
+    elif (file_encoding == speech.RecognitionConfig.AudioEncoding.FLAC):
         encoding = 'FLAC'
-
-    if detect_lang == "en-US":
+    print("language_check : ",language_check)
+    if language_check == "en-US":
         config = {
-            "language_code": detect_lang,
+            "language_code": language_check,
             "model": "medical_dictation",
             "encoding": encoding,
-            "sample_rate_hertz": multi_lan.sample_rate
+            "sample_rate_hertz": sample_rate
         }
 
-    elif detect_lang == "ko-KR" or "ja-JP":
+    elif language_check == "ko-KR" or "ja-JP":
         config = {
-            "language_code": detect_lang,
+            "language_code": language_check,
             "encoding": encoding,
-            "sample_rate_hertz": multi_lan.sample_rate
+            "sample_rate_hertz": sample_rate
         }
 
     response = client.recognize(config=config, audio=audio)
     save_response_as_json(response, output_json_file)
 
     # print(f"json 파일 생성 완료: {output_json_file}")
+
 
 def save_response_as_json(response, output_file):
     results = []
