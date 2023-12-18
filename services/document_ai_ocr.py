f90d960e9a5db3e0ba430f60d5a914c118b164fb
minji0916 <a2927036@naver.com>
2023-12-19 05:25:41 +0900

    feat: 일본어 문진표 ocr
    일본어 문진표 ocr 가능하도록 기능 추가
    assets폴더에 문진표 예시 둠
    
    vertex ai의 결과가 256토큰을 초과할 경우 에러가 발생해서 1024 토큰으로 증가(에러 해결)
    
    현재 문제상황)
    문진표의 체크박스는 아예 인식하지 못한다. 심지어 문진표에 작성한 내용을 바탕으로 vertex ai가 말을 지어낸다.
    
    해결 방법)
    1. 체크박스 인식하는 모델 추가 개발
    2. 체크박스 없는 문진표 찾아서 데모 진행

--- services/document_ai_ocr.py
+++ services/document_ai_ocr.py
@@ -5,7 +5,7 @@ from flask import current_app
 from services.vertexai_text_command import text_generation
 from services.filesave import save_file
 from .settings import get_secret, get_projectId
-# from services.language_detection import detect_language
+import langid
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
@@ -46,22 +46,47 @@ def process_document(file_path: str, mime_type: str) -> str:
     
     return result.document.text
 
+def detect_language(text):
+    """
+    문자열의 언어 감지 
+    (langid 라이브러리 사용 -> language, confidence가 tuple 형식으로 묶여서 return됨)
+    """
+    lang = langid.classify(text)[0]
+    return lang
+
 def get_ocr_json(pdf_file_name, saved_ocr_name):
     # 문진표 파일 정보
     file_path = os.path.join(current_dir, "voice", pdf_file_name)
 
     # 문진표 OCR 진행
     text = process_document(file_path=file_path, mime_type='application/pdf')
+    print("text: ", text)
+
+    # 문진표 언어 감지
+    lang = detect_language(text)
+    print("language: ", lang)
 
     # OCR 진행한 파일에서 특정 정보 추출
-    doc_info_export_command = """Please compile information pertaining to \
-            'NAME, DATE OF SERVICE, DATE OF BIRTH, DOCTOR, PATIENT, CHIEF COMPLAINT, ONSET OF SYMPTOMS, \
-            MECHANISM OF INJURY, What makes the pain better?, What makes the pain worse?' \
-            and Pair each key with its corresponding value and format it in JSON:"""
+    if lang == 'en':
+        doc_info_export_command = """Please compile information pertaining to \
+                'NAME, DATE OF SERVICE, DATE OF BIRTH, DOCTOR, PATIENT, CHIEF COMPLAINT, ONSET OF SYMPTOMS, \
+                MECHANISM OF INJURY, What makes the pain better?, What makes the pain worse?' \
+                and Pair each key with its corresponding value and format it in JSON:"""
+        
+    elif lang == 'ja':
+        doc_info_export_command = """Please compile information pertaining to \
+            "Name, DATE OF SERVICE, Date of Birth, Address (Postal Code), Phone Number, Height, Weight, Gender, Body Temperature, \
+            What symptoms are you experiencing today?, When did these symptoms start?, \
+            Please check the applicable symptoms, Do you have any allergies to medications or food?, \
+            Have you had any previous illnesses?, Are you currently undergoing treatment for any illness?, Are you currently taking any medication?"
+            and pair each key with its corresponding value and format it in JSON:"""
+
     doc_info_export_result = text_generation(0.0, 'us-central1', text, doc_info_export_command)
+    print("doc_info_export_result: ", doc_info_export_result)
 
     # 텍스트를 JSON으로 변환
     doc_info_export_result_json = json.loads(doc_info_export_result.replace('json', '').replace("```", ''))
+    print("doc_info_export_result_json: ", doc_info_export_result_json)
 
     # 파일 저장
     file_path = os.path.join(current_dir, "voice")
--- services/vertexai_text_command.py
+++ services/vertexai_text_command.py
@@ -29,7 +29,7 @@ def text_generation(
     
     parameters = {
         "temperature": temperature,  # Temperature controls the degree of randomness in token selection.
-        "max_output_tokens": 256,  # Token limit determines the maximum amount of text output.
+        "max_output_tokens": 1024,  # Token limit determines the maximum amount of text output.
         "top_p": 0.95,
         # Tokens are selected from most probable to least until the sum of their probabilities equals the top_p value.
         "top_k": 40,  # A top_k of 1 means the selected token is the most probable among all tokens.
